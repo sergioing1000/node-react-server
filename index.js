b99ebac2534b0875bea201145e07a205809d80ae
Sergio Cruz <sergioing@yahoo.com>
2024-12-14 23:13:36 -0500

    renamed:    server.js -> index.js 	modified:   package.json 	modified:   vercel.json

--- index.js
+++ index.js
@@ -111,8 +111,8 @@ async function saveDocuments(data) {
 ////////// ROUTES //////////
 // Get all items
 app.get("/", (req, res) => {
-  
   const htmlresponse = '<html><head><title>Document</title></head><body><h2>Title</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi velit, eligendi nihil dolores odio deleniti officia labore veniam fuga quaerat totam voluptate dolore consectetur reiciendis error quos quae, fugit repellat.</p></body></html>';
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(htmlresponse);
 });
 
