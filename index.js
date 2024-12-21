cc10e5a968d6abe5c574cbbd229f881f662a1300
Sergio Cruz <sergioing@yahoo.com>
2024-12-20 21:50:23 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const corsOptions = {
   origin: "https://zingy-frangollo-f59cf4.netlify.app",
   // origin: "http://localhost:5173/",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 // Middleware
