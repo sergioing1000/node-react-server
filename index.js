86289566634a2dd7e2e56e8b1790abe81bc830ff
Sergio Cruz <sergioing@yahoo.com>
2024-12-24 12:56:33 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -143,16 +143,16 @@ app.get("/", (req, res) => {
   res.send(htmlresponse);
 });
 
-app.post("/", (req, res) => {
-  const htmlresponse =
-    "<html><head><title>Document</title></head><body><h2>Title</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi velit, eligendi nihil dolores odio deleniti officia labore veniam fuga quaerat totam voluptate dolore consectetur reiciendis error quos quae, fugit repellat.</p></body></html>";
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://wish-list-apeh.vercel.app",
-    // "http://localhost:5173"
-  );
-  res.send(htmlresponse);
-});
+// app.post("/", (req, res) => {
+//   const htmlresponse =
+//     "<html><head><title>Document</title></head><body><h2>Title</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi velit, eligendi nihil dolores odio deleniti officia labore veniam fuga quaerat totam voluptate dolore consectetur reiciendis error quos quae, fugit repellat.</p></body></html>";
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://wish-list-apeh.vercel.app",
+//     // "http://localhost:5173"
+//   );
+//   res.send(htmlresponse);
+// });
 
 // Get all items
 app.get("/api/items", (req, res) => {
